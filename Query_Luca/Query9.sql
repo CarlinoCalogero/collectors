2e9f8b85905cbd2ca9c7fa9743eb40887d038ba0
LucaFraMacera <lucamacera02@gmail.com>
2023-05-27 12:00:42 +0200

    Finilized query 9. Needs a check on efficiency

--- Query_Luca/Query9.sql
+++ Query_Luca/Query9.sql
@@ -1,4 +1,21 @@
-select coll.id_collezionista, coll.*, cls.nickname
-from condivisa cond
-join collezione_di_dischi coll on cond.id_collezione = coll.id
-join collezionista cls on cond.id_collezionista = cls.id
+drop procedure if exists collezioni_visibili_by;
+delimiter $
+create procedure collezioni_visibili_by(id_collezionista integer unsigned)
+begin
+	#Visualizzare collezioni personali
+	select coll.nome, coll.visibilita
+	from collezione_di_dischi coll
+	join collezionista c on coll.id_collezionista = c.id
+	where c.id = 3
+	#Visualizzare le collezioni condivise
+	union distinct 
+	select coll.nome, coll.visibilita
+	from condivisa cond
+	join collezione_di_dischi coll on cond.id_collezione = coll.id
+	join collezionista cls on cond.id_collezionista = cls.id
+	where cond.id_collezionista = 3
+	#Collezioni Pubbliche
+	union distinct
+	select cp.nome,cp.visibilita from collezioni_pubbliche cp;
+end$
+delimiter ;
--- Query_Luca/view_collezioni_pubbliche.sql
+++ Query_Luca/view_collezioni_pubbliche.sql
@@ -1,5 +1,5 @@
 drop view if exists collezioni_pubbliche;
 create view collezioni_pubbliche as
-		select id, nome
+		select nome,visibilita
 		from collezione_di_dischi
 		where visibilita = true;
