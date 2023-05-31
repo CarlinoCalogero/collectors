bb2c22d4317c673da27d41e00b5ca3a087cce3e1
Mik1810 <michael.piccirilli@student.univaq.it>
2023-05-31 10:29:01 +0200

    Corrected logical error in quey 10 and 11

--- Query_Mik/10 FUNCTION_conta_brani.sql
+++ Query_Mik/10 FUNCTION_conta_brani.sql
@@ -11,17 +11,15 @@ BEGIN
 	DECLARE numero_brani integer unsigned;
     
     SET numero_brani = (
-		SELECT count(*)
-		FROM (
-			SELECT t.id
-            FROM autore a
-			JOIN produce p ON p.id_autore=a.id
-			JOIN traccia t ON t.id=p.id_traccia
-			JOIN disco d ON d.id=t.id_disco
-			JOIN collezione_di_dischi c ON c.id=d.id_collezione_di_dischi
-			WHERE c.visibilita=true 
-			AND a.nome_darte=nome_darte
-			GROUP BY t.id) as brani_distinti);
+		SELECT count(distinct t.id)
+		FROM autore a
+		JOIN produce p ON p.id_autore=a.id
+		JOIN traccia t ON t.id=p.id_traccia
+		JOIN disco d ON d.id=t.id_disco
+		JOIN collezione_di_dischi c ON c.id=d.id_collezione_di_dischi
+		WHERE c.visibilita=true 
+		AND a.nome_darte=nome_darte
+		GROUP BY a.id);
 	
     RETURN numero_brani;
 
--- Query_Mik/11 FUNCTION_conta_minuti_autore.sql
+++ Query_Mik/11 FUNCTION_conta_minuti_autore.sql
@@ -12,17 +12,15 @@ BEGIN
 	DECLARE minutaggio_totale decimal(10,2);
 	
     SET minutaggio_totale = (
-		SELECT sum(brani_distinti.durata)
-		FROM ( # Cerco le tracce di un autore
-			SELECT t.id,t.durata
-            FROM autore a
-			JOIN produce p ON p.id_autore=a.id
-			JOIN traccia t ON t.id=p.id_traccia
-			JOIN disco d ON d.id=t.id_disco
-			JOIN collezione_di_dischi c ON c.id=d.id_collezione_di_dischi
-			WHERE c.visibilita=true 
-			AND a.nome_darte=nome_darte
-			GROUP BY t.id) as brani_distinti);
+		SELECT sum(distinct t.durata)
+		FROM autore a
+		JOIN produce p ON p.id_autore=a.id
+		JOIN traccia t ON t.id=p.id_traccia
+		JOIN disco d ON d.id=t.id_disco
+		JOIN collezione_di_dischi c ON c.id=d.id_collezione_di_dischi
+		WHERE c.visibilita=true 
+		AND a.nome_darte=nome_darte
+		GROUP BY a.id);
             
 	RETURN minutaggio_totale;
 
