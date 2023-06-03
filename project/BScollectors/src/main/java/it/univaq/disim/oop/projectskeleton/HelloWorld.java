e46a095eebbde9a8af3ecae632a68946d50f173d
CarlinoCalogero <calogerocarlino19@gmail.com>
2023-06-03 10:56:55 +0200

    initial API project commit

---
+++ project/BScollectors/src/main/java/it/univaq/disim/oop/projectskeleton/HelloWorld.java
@@ -0,0 +1,32 @@
+package it.univaq.disim.oop.projectskeleton;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+		
+	}
+
+}
+
+
+/* 
+ * VM Arguments:
+ * --module-path "C:\Program Files (x86)\JavaFX versions\javafx-sdk-18.0.1\lib" --add-modules javafx.controls,javafx.fxml --add-modules javafx.controls,javafx.media
+ */
