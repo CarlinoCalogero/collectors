6e9bd348ab31d62088732cebca130b1e782f8fba
Mik1810 <michael.piccirilli@student.univaq.it>
2023-06-06 13:54:17 +0200

    Added the save function to save a collection in the db

--- project/BScollectors/src/main/java/it/univaq/disim/oop/collectors/controllers/InsertCollectionController.java
+++ project/BScollectors/src/main/java/it/univaq/disim/oop/collectors/controllers/InsertCollectionController.java
@@ -3,14 +3,25 @@ package it.univaq.disim.oop.collectors.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import it.univaq.disim.oop.collectors.business.BusinessFactory;
+import it.univaq.disim.oop.collectors.business.JBCD.DatabaseConnectionException;
+import it.univaq.disim.oop.collectors.business.JBCD.Query_JDBC;
+import it.univaq.disim.oop.collectors.domain.Collection;
+import it.univaq.disim.oop.collectors.domain.Collector;
 import it.univaq.disim.oop.collectors.viste.DataInitalizable;
+import it.univaq.disim.oop.collectors.viste.ViewDispatcher;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
-public class InsertCollectionController<T> implements Initializable,DataInitalizable<T>{
+public class InsertCollectionController implements Initializable,DataInitalizable<Collector>{
+	
+	private ViewDispatcher dispatcher = ViewDispatcher.getInstance();
+	private Query_JDBC implementation = BusinessFactory.getImplementation();
+	
+	private Collector collector;
 	
 	@FXML
 	private Button saveButton;
@@ -26,13 +37,17 @@ public class InsertCollectionController<T> implements Initializable,DataInitaliz
 		
 	}
 	
-	public void initializeData(T t) {
-		
+	public void initializeData(Collector collector) {
+		this.collector = collector;
 	}
 	
 	@FXML
-	private void save() {
-		
+	private void save() throws DatabaseConnectionException {
+		if(publicRadioButton.isSelected()) {
+			implementation.insertCollezione(new Collection(null,nameTextField.getText(),true,collector.getID()));
+		} else if (privateRadioButton.isSelected()) {
+			
+		} else System.err.println("La visibilità deve essere selezionata!");
 	}
 
 }
